5416bd6d3e318d5365545a0e93ee97b332b0c0e5
AlvaroMarques <sc.poi458@gmail.com>
2026-01-11 13:40:46 -0300

    Adding metadata on the  API

--- src/orchestrator/assets.py
+++ src/orchestrator/assets.py
@@ -1,4 +1,4 @@
-from dagster import asset, asset_check
+from dagster import asset, asset_check, Output, MetadataValue
 import subprocess
 
 from pathlib import Path, PosixPath
@@ -6,7 +6,7 @@ from tempfile import mkdtemp
 
 
 @asset
-def scrape() -> PosixPath:
+def scrape() -> Output[PosixPath]:
     directory = mkdtemp()
     process = subprocess.Popen(
         ["uv", "run", "--project=../scraper", "cvm", "2", directory, "metadata.json"],
@@ -18,11 +18,13 @@ def scrape() -> PosixPath:
     # communicate() sends data to stdin (if provided) and reads data from stdout and stderr until the process terminates
     stdout, stderr = process.communicate()
 
-    return Path(directory) / "metadata.json"
+    metadata_path = Path(directory) / "metadata.json"
+
+    return Output(value=metadata_path, metadata={"metadata_path": metadata_path})
 
 
 @asset
-def make_reports(scrape: PosixPath):
+def make_reports(scrape: PosixPath) -> Output[PosixPath]:
     output_file = scrape.parent / "output.json"
     process = subprocess.Popen(
         [
@@ -41,4 +43,4 @@ def make_reports(scrape: PosixPath):
 
     # communicate() sends data to stdin (if provided) and reads data from stdout and stderr until the process terminates
     stdout, stderr = process.communicate()
-    return output_file
+    return Output(value=output_file, metadata={"output_file": output_file})
